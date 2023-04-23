edfc6a67c46def3e93d59447f71245d2484ff220
lllilllilllilili <hik3562@hufs.ac.kr>
2023-04-23 22:18:59 +0900

    feature: 객체 생성 방법

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
     println("Hello World!")
-
+    //코틀린에서는 null을 진짜 싫어하는구나..
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
@@ -24,4 +24,20 @@ fun main(args: Array<String>) {
     //상황에 따라서 내부적으로 Primitive 로 바꿔서 알아서 처리해준다.
     var test1 = 10L;
     var test2 = 1_00L;
+
+    var number3 : Long? = 10L // 내부적으로 알아서 바꿔준다.
+    number3 = null; //null이 들어갈 수 없게 설계 : Long? 이렇게 써야 한다. 
+    
+    //TODO(inguu): 메모, 프로그래머가 boxing / unboxing 을 고려하지 않아도 되도록 Kotlin이 알아서 처리해준다. 
+    
+    
+    var person = Person("inguu") //new를 안붙여도 객체를 생성할 수 있다.
+    //var : 변경 가능하다. val : 변경 불가능하다. (read-only)
+    //타입을 명시적을 작성하지 않아도 타입이 추론된다.
 }
+
+//TODO(inguu) : 코틀린에서 변수와 타입, 연산자를 다루는 방법
+/**
+ * Sub Mission : 계산기 하나 만들어보자.
+ */
+
