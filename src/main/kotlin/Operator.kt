382d0c19f64847fde4e8bf7a58afea4d1a749d5a
lllilllilllilili <hik3562@hufs.ac.kr>
2023-04-24 23:57:20 +0900

    feature: Operator 코틀린, 자바 차이점 작성 (==, ===)

--- src/main/kotlin/Operator.kt
+++ src/main/kotlin/Operator.kt
@@ -7,4 +7,20 @@ fun main() {
     if (money1 > money2) {
         println("Money1이 Money2보다 금액이 큽니다.");
     }
+
+    /**
+     *
+     * 동등성 : 두 객체의 값이 같은가 -> equals -> ==
+     * 동일성 : == (주소, 자바) -> === (코틀린)
+     */
+
+    val money3 = JavaMoney(1_000L)
+    val money5 = money3
+    val money6 = JavaMoney(3_000L)
+
+    //주소가 같은지
+    println(money3 === money5)
+    //값이 같은지
+    println(money3 == money5)
+
 }
