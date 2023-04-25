7ba9ec0300f94b875213b2ebe487903882ef5807
lllilllilllilili <hik3562@hufs.ac.kr>
2023-04-25 23:26:10 +0900

    feature: || 연산자에서 작동하는 방법

---
+++ src/main/kotlin/Lazy.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    if (fun1() || fun2()) {
+        println("본문");
+    }
+
+}
+
+fun fun1(): Boolean {
+    println("fun 1");
+    return true;
+}
+
+fun fun2(): Boolean {
+    println("fun 2");
+    return false;
+}
