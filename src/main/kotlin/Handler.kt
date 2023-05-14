45cf347ee7887fa95ce5e692059d7a41f4132cef
lllilllilllilili <hik3562@hufs.ac.kr>
2023-05-14 23:21:28 +0900

    feature: return으로 expression을 묶을 수 있음

--- src/main/kotlin/Handler.kt
+++ src/main/kotlin/Handler.kt
@@ -10,9 +10,21 @@ Java에서 if-else는 Statement
 Kotlin에서는 Expression
  */
 fun getPassOrFail(score: Int): String {
-    if (score >= 50) {
+    return if (score >= 50) {
         return "P";
     } else {
         return "F";
+    }
+}
+
+fun getGrade(score: Int): String {
+    return if (score > 90) {
+         "A"
+    } else if (score >= 80) {
+         "B"
+    } else if (score >= 70) {
+         "D"
+    } else {
+        "E"
     }
 }
